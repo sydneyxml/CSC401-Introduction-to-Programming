35f0f46ddd880ced1aacf3ed7caf156da16e69b7
Ximan (Sydney) Liu <61263191+sydneyxml@users.noreply.github.com>
2020-07-14 04:30:41 -0500

    Add files via upload

---
+++ HW2/2E.py
@@ -0,0 +1,23 @@
+
+# 创建函数 function
+
+#Problem E
+
+def average(some_scores):
+    'Accepts a list of scores and returns their average'
+    print("hello")
+    return sum(some_scores) / len(some_scores)
+
+help(average)
+
+num_scores = eval(input('Enter number of scores: '))
+
+score_list = []
+for num in range(num_scores):   # num_scores被eval过，此时是个数字
+    score = eval(input('Enter score value: '))
+    score_list.append(score)
+
+results = average(score_list)
+
+print("Average:", results)
+
